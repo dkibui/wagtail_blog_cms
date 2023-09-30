8ca914c5f3b9d35d87ac90bcebabc68636fdc004
David Kibui <dkibui@outlook.com>
2023-09-30 19:06:19 +0300

    Initial commit

---
+++ main/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-ix=sq4clvtdxzr9=lz2kr6nu8lp--#*6s3i4g9j*084!a-gj_g"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
