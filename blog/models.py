29131225da4e6228486eeb66e1286693982c57de
David Kibui <dkibui@outlook.com>
2023-09-30 20:28:17 +0300

    Added blog models to blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from wagtail.admin.panels import FieldPanel
+from wagtail.models import Page
+from wagtail.images.blocks import ImageChooserBlock
 
-# Create your models here.
+
+class BlogPage(Page):
+    description = models.CharField(max_length=255, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("description"),
+    ]
+
+
+class PostPage(Page):
+    header_image = models.ForeignKey("wagtailimages.Image", on_delete=models.SET_NULL, null=True, blank=True, related_name="+")
+
+    content_panels = Page.content_panels + [
+        FieldPanel("header_image"),
+    ]
