1d8da1ec8780cf741af0fccf7451d3a2caad5ccb
David Kibui <dkibui@outlook.com>
2023-10-02 20:39:37 +0300

    Changes to blog app

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-from django.db.models import SET_NULL, CASCADE
+from django.db.models import CASCADE, SET_NULL
 from modelcluster.fields import ParentalKey
-from wagtail.admin.panels import FieldPanel
+from taggit.managers import TaggableManager
+from taggit.models import Tag as TaggitTag
+from taggit.models import TaggedItemBase
+from wagtail.admin.panels import FieldPanel, InlinePanel
 from wagtail.models import Page
 from wagtail.snippets.models import register_snippet
-from taggit.models import Tag as TaggitTag
 
 
 class BlogPage(Page):
@@ -16,17 +18,31 @@ class BlogPage(Page):
 
 
 class PostPage(Page):
-    header_image = models.ForeignKey("wagtailimages.Image", on_delete=models.SET_NULL, null=True, blank=True,
+    header_image = models.ForeignKey("wagtailimages.Image", on_delete=SET_NULL, null=True, blank=True,
                                      related_name="+")
+    tags = TaggableManager(through="PostPageTags", blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel("header_image"),
+        FieldPanel("tags"),
+        InlinePanel("categories", label="category"),
     ]
 
 
 class PostPageBlogCategory(models.Model):
     page = ParentalKey("blog.PostPage", on_delete=CASCADE, blank=True, related_name="categories")
-    blog_category = models.ForeignKey("BlogCategory", on_delete=models.CASCADE, blank=True, related_name="post_pages")
+    blog_category = models.ForeignKey("BlogCategory", on_delete=CASCADE, blank=True, related_name="post_pages")
+
+    panels = [
+        FieldPanel("blog_category"),
+    ]
+
+    class Meta:
+        unique_together = ("page", "blog_category",)
+
+
+class PostPageTags(TaggedItemBase):
+    content_object = ParentalKey("blog.PostPage", blank=True)
 
 
 @register_snippet
--- home/models.py
+++ home/models.py
@@ -1,7 +1,16 @@
 from django.db import models
-
 from wagtail.models import Page
+from wagtail.fields import RichTextField
 
 
 class HomePage(Page):
-    pass
+    title = models.TextField()
+    body = RichTextField()
+    date = models.DateField("Post date")
+    feed_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
